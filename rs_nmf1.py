8772e8f008f9c122d2c3c089ff8e463a084350b0
Thon-Da Nguyen <67877012+da1nguyen@users.noreply.github.com>
2023-06-13 14:48:51 +0700

    Update rs_nmf1.py

--- rs_nmf1.py
+++ rs_nmf1.py
@@ -1,27 +1,24 @@
-# Khai báo các thư viện
 import pandas as pd
 import streamlit as st
 import requests
 import io
 
-# Chỉ ra dữ liệu (ở đây chứa một file csv)
+# Khai báo URL dữ liệu
 data_url = 'https://drive.google.com/uc?id=1MHLvwXQMgRKz9BMYqNE-NxPVUfoEmoYJ'
 
-# Yêu cầu dữ liệu từ link kết url trên
+# Yêu cầu dữ liệu từ URL
 response = requests.get(data_url)
 
-# Kiểm tra xem link có thể nhận về trực tiếp hay không
+# Kiểm tra xem có lỗi trong quá trình tải dữ liệu không
 assert response.status_code == 200, 'Could not download the data'
 
 # Đọc dữ liệu vào DataFrame
 data = pd.read_csv(io.StringIO(response.content.decode('utf-8')))
 pd.set_option('display.max_colwidth', None)
 
-# Chọn chỉ mục của các cột muốn hiển thị
-selected_columns = ['reviewerID', 'asin', 'overall']
-
-# Hiển thị DataFrame với các cột đã chọn
-st.dataframe(data[selected_columns])
+# Hiển thị các cột của DataFrame
+selected_columns = [1, 2, 4, 5, 7]
+st.dataframe(data.iloc[:, selected_columns])
 
 from surprise import Dataset, Reader
 from surprise.model_selection import cross_validate
@@ -31,7 +28,7 @@ from surprise import NMF
 reader = Reader(rating_scale=(1, 5))
 
 # Tạo một đối tượng Dataset từ DataFrame
-dataset = Dataset.load_from_df(data[['reviewerID', 'asin', 'overall']], reader)
+dataset = Dataset.load_from_df(data[['userID', 'itemID', 'rating']], reader)
 
 # Xây dựng mô hình NMF với số lượng yếu tố latents = 10
 model = NMF(n_factors=10)
@@ -39,9 +36,9 @@ model = NMF(n_factors=10)
 # Đào tạo mô hình trên dữ liệu
 cross_validate(model, dataset, measures=['RMSE', 'MAE'], cv=5, verbose=True)
 
-# Lấy ID người dùng đầu vào từ người dùng
+# Lấy đầu vào từ người dùng
 user_id = st.text_input("Nhập ID người dùng:")
-k = int(st.text_input("Nhập số lượng sản phẩm khuyến nghị:"))
+k = st.slider("Chọn số lượng sản phẩm khuyến nghị:", 1, 10, 3)
 
 # Đào tạo mô hình trên toàn bộ dữ liệu
 trainset = dataset.build_full_trainset()
@@ -56,13 +53,11 @@ predictions = model.test(items_to_recommend)
 # Sắp xếp dự đoán theo xếp hạng giảm dần
 top_k_predictions = sorted(predictions, key=lambda x: x.est, reverse=True)[:k]
 
-# Lấy danh sách sản phẩm được khuyến nghị
-recommended_items = [(pred.iid, pred.est) for pred in top_k_predictions]
-recommended_df = pd.DataFrame(recommended_items, columns=['asin', 'rating'])
+# Tạo DataFrame cho sản phẩm được khuyến nghị và xếp hạng tương ứng
+recommended_df = pd.DataFrame({'Sản phẩm': [pred.iid for pred in top_k_predictions],
+                              'Xếp hạng': [pred.est for pred in top_k_predictions]})
 
-# Merge thông tin về sản phẩm từ DataFrame gốc
-recommended_df = recommended_df.merge(data[['asin', 'reviewerID']], on='asin', how='left')
-
+# Hiển thị bảng dữ liệu của các sản phẩm được khuyến nghị
 st.write("Top", k, "sản phẩm được khuyến nghị:")
 st.dataframe(recommended_df)
 
