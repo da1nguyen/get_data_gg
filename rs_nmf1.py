c863058cd7f313be52a07748248632a50367bb23
Thon-Da Nguyen <67877012+da1nguyen@users.noreply.github.com>
2023-06-13 14:03:32 +0700

    Update rs_nmf1.py

--- rs_nmf1.py
+++ rs_nmf1.py
@@ -4,7 +4,7 @@ import requests
 import io
 from surprise import Dataset, Reader, NMF
 
-# Chỉ ra dữ liệu (ở đây chứa một file csv)
+# Chỉ ra dữ liệu (ở đây chứa một file csv) 
 data_url = 'https://drive.google.com/uc?id=1MHLvwXQMgRKz9BMYqNE-NxPVUfoEmoYJ'
 
 # Yêu cầu dữ liệu từ link kết url trên
