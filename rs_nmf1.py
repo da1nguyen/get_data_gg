de956877251c503a629b12e78d09cb2b486ecaf0
Thon-Da Nguyen <67877012+da1nguyen@users.noreply.github.com>
2023-06-13 14:08:54 +0700

    Update rs_nmf1.py

--- rs_nmf1.py
+++ rs_nmf1.py
@@ -4,7 +4,7 @@ import requests
 import io
 from surprise import Dataset, Reader, NMF
 
-# Chỉ ra dữ liệu (ở đây chứa một file csv) 
+# Chỉ ra dữ liệu (ở đây chứa một file csv)
 data_url = 'https://drive.google.com/uc?id=1MHLvwXQMgRKz9BMYqNE-NxPVUfoEmoYJ'
 
 # Yêu cầu dữ liệu từ link kết url trên
@@ -35,7 +35,7 @@ dataset = Dataset.load_from_df(data[['userID', 'ProductID', 'rating']], reader)
 # Xây dựng mô hình NMF với số lượng yếu tố latents = 10
 model = NMF(n_factors=10)
 
-# Đào tạo mô hình trên toàn bộ dữ liệu ...
+# Đào tạo mô hình trên toàn bộ dữ liệu
 trainset = dataset.build_full_trainset()
 model.fit(trainset)
 
@@ -53,8 +53,9 @@ if st.button("Khuyến nghị"):
     # Sắp xếp dự đoán theo xếp hạng giảm dần
     top_k_predictions = sorted(predictions, key=lambda x: x.est, reverse=True)[:k]
 
-    # Hiển thị danh sách sản phẩm được khuyến nghị
-    recommended_items = [pred.iid for pred in top_k_predictions]
-    recommended_df = data[data['ProductID'].isin(recommended_items)]
+    # Hiển thị danh sách sản phẩm được khuyến nghị cùng với điểm số
+    recommended_items = [(pred.iid, pred.est) for pred in top_k_predictions]
+    recommended_df = pd.DataFrame(recommended_items, columns=['ProductID', 'Score'])
+    recommended_df = recommended_df.merge(data[['ProductID', 'itemName']], on='ProductID', how='left')
     st.write("Top", k, "sản phẩm được khuyến nghị:")
     st.dataframe(recommended_df)
