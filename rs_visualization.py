a5ef32d03a5b5a59d3f09b27b0073413dea20267
Thon-Da Nguyen <67877012+da1nguyen@users.noreply.github.com>
2023-06-13 20:38:45 +0700

    Update rs_visualization.py

--- rs_visualization.py
+++ rs_visualization.py
@@ -4,16 +4,29 @@ import numpy as np
 from sklearn.decomposition import NMF
 import gdown
 
-# Tải dữ liệu từ Google Drive
-url = 'https://drive.google.com/uc?id=1MHLvwXQMgRKz9BMYqNE-NxPVUfoEmoYJ'
-output = 'data.csv'
-gdown.download(url, output, quiet=False)
+import pandas as pd
+import streamlit as st
+import requests
+import io
+from surprise import Dataset, Reader, NMF
 
-# Đọc dữ liệu
-df = pd.read_csv('data.csv')
+# Khai báo các thư viện
+
+# Chỉ ra dữ liệu (ở đây chứa một file csv)
+data_url = 'https://drive.google.com/uc?id=1MHLvwXQMgRKz9BMYqNE-NxPVUfoEmoYJ'
+
+# Yêu cầu dữ liệu từ link kết url trên
+response = requests.get(data_url)
+
+# Kiểm tra xem link có thể nhận về trực tiếp hay không
+assert response.status_code == 200, 'Could not download the data'
+
+# Đọc dữ liệu vào DataFrame
+data = pd.read_csv(io.StringIO(response.content.decode('utf-8')))
+pd.set_option('display.max_colwidth', None)
 
 # Tạo ma trận đánh giá
-R = df.values
+R = data.values
 
 # Thực hiện NMF
 model = NMF(n_components=5, init='random', random_state=0)
