a81063545867ace2738f73a8c32a529e667b7e77
Thon-Da Nguyen <67877012+da1nguyen@users.noreply.github.com>
2023-06-13 14:54:51 +0700

    Update rs_nmf1.py

--- rs_nmf1.py
+++ rs_nmf1.py
@@ -16,19 +16,20 @@ assert response.status_code == 200, 'Could not download the data'
 data = pd.read_csv(io.StringIO(response.content.decode('utf-8')))
 pd.set_option('display.max_colwidth', None)
 
-# Hiển thị các cột của DataFrame
-selected_columns = [1, 2, 4, 5, 7]
-st.dataframe(data.iloc[:, selected_columns])
+# Chọn chỉ mục của các cột muốn hiển thị
+selected_columns = ['reviewerID', 'asin', 'overall']
 
-from surprise import Dataset, Reader
-from surprise.model_selection import cross_validate
-from surprise import NMF
+# Hiển thị DataFrame với các cột đã chọn
+st.dataframe(data[selected_columns])
+
+# Tiếp tục với phần còn lại của code
+from surprise import Dataset, Reader, NMF
 
 # Tạo một đối tượng Reader để định dạng dữ liệu
 reader = Reader(rating_scale=(1, 5))
 
 # Tạo một đối tượng Dataset từ DataFrame
-dataset = Dataset.load_from_df(data[['userID', 'itemID', 'rating']], reader)
+dataset = Dataset.load_from_df(data[['reviewerID', 'asin', 'overall']], reader)
 
 # Xây dựng mô hình NMF với số lượng yếu tố latents = 10
 model = NMF(n_factors=10)
@@ -36,9 +37,9 @@ model = NMF(n_factors=10)
 # Đào tạo mô hình trên dữ liệu
 cross_validate(model, dataset, measures=['RMSE', 'MAE'], cv=5, verbose=True)
 
-# Lấy đầu vào từ người dùng
+# Lấy ID người dùng đầu vào từ người dùng
 user_id = st.text_input("Nhập ID người dùng:")
-k = st.slider("Chọn số lượng sản phẩm khuyến nghị:", 1, 10, 3)
+k = int(st.text_input("Nhập số lượng sản phẩm khuyến nghị:"))
 
 # Đào tạo mô hình trên toàn bộ dữ liệu
 trainset = dataset.build_full_trainset()
@@ -53,11 +54,9 @@ predictions = model.test(items_to_recommend)
 # Sắp xếp dự đoán theo xếp hạng giảm dần
 top_k_predictions = sorted(predictions, key=lambda x: x.est, reverse=True)[:k]
 
-# Tạo DataFrame cho sản phẩm được khuyến nghị và xếp hạng tương ứng
-recommended_df = pd.DataFrame({'Sản phẩm': [pred.iid for pred in top_k_predictions],
-                              'Xếp hạng': [pred.est for pred in top_k_predictions]})
-
-# Hiển thị bảng dữ liệu của các sản phẩm được khuyến nghị
+# Hiển thị danh sách sản phẩm được khuyến nghị
+recommended_items = [pred.iid for pred in top_k_predictions]
+recommended_df = data[data['asin'].isin(recommended_items)][['asin', 'overall']]
+recommended_df = recommended_df.drop_duplicates(subset=['asin'])
 st.write("Top", k, "sản phẩm được khuyến nghị:")
 st.dataframe(recommended_df)
-
