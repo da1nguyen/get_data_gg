41d6a184b030f3fbe6ce3b5a4b08c1a20ec8c96a
Thon-Da Nguyen <67877012+da1nguyen@users.noreply.github.com>
2023-06-13 09:43:19 +0700

    Update rs_nmf.py

--- rs_nmf.py
+++ rs_nmf.py
@@ -14,39 +14,42 @@ assert response.status_code == 200, 'Could not download the data'
 
 # Đọc dữ liệu vào DataFrame
 data = pd.read_csv(io.StringIO(response.content.decode('utf-8')))
+
+st.dataframe(data )
+
 from surprise import Dataset, Reader
 from surprise.model_selection import cross_validate
 from surprise import NMF
 
-# Tạo một đối tượng Reader để định dạng dữ liệu
-reader = Reader(rating_scale=(1, 5))
+# # Tạo một đối tượng Reader để định dạng dữ liệu
+# reader = Reader(rating_scale=(1, 5))
 
-# Tạo một đối tượng Dataset từ DataFrame
-dataset = Dataset.load_from_df(data[['userID', 'itemID', 'rating']], reader)
+# # Tạo một đối tượng Dataset từ DataFrame
+# dataset = Dataset.load_from_df(data[['userID', 'itemID', 'rating']], reader)
 
-# Xây dựng mô hình NMF với số lượng yếu tố latents = 10
-model = NMF(n_factors=10)
+# # Xây dựng mô hình NMF với số lượng yếu tố latents = 10
+# model = NMF(n_factors=10)
 
-# Đào tạo mô hình trên dữ liệu
-cross_validate(model, dataset, measures=['RMSE', 'MAE'], cv=5, verbose=True)
-# Lấy ID người dùng đầu vào từ người dùng
-user_id = st.text_input("Nhập ID người dùng:")
-k = int(st.text_input("Nhập số lượng sản phẩm khuyến nghị:"))
+# # Đào tạo mô hình trên dữ liệu
+# cross_validate(model, dataset, measures=['RMSE', 'MAE'], cv=5, verbose=True)
+# # Lấy ID người dùng đầu vào từ người dùng
+# user_id = st.text_input("Nhập ID người dùng:")
+# k = int(st.text_input("Nhập số lượng sản phẩm khuyến nghị:"))
 
-# Đào tạo mô hình trên toàn bộ dữ liệu
-trainset = dataset.build_full_trainset()
-model.fit(trainset)
+# # Đào tạo mô hình trên toàn bộ dữ liệu
+# trainset = dataset.build_full_trainset()
+# model.fit(trainset)
 
-# Lấy danh sách sản phẩm chưa được người dùng đánh giá
-items_to_recommend = trainset.build_anti_testset().for_user(user_id)
+# # Lấy danh sách sản phẩm chưa được người dùng đánh giá
+# items_to_recommend = trainset.build_anti_testset().for_user(user_id)
 
-# Dự đoán xếp hạng cho sản phẩm chưa được đánh giá
-predictions = model.test(items_to_recommend)
+# # Dự đoán xếp hạng cho sản phẩm chưa được đánh giá
+# predictions = model.test(items_to_recommend)
 
-# Sắp xếp dự đoán theo xếp hạng giảm dần
-top_k_predictions = sorted(predictions, key=lambda x: x.est, reverse=True)[:k]
+# # Sắp xếp dự đoán theo xếp hạng giảm dần
+# top_k_predictions = sorted(predictions, key=lambda x: x.est, reverse=True)[:k]
 
-# Hiển thị danh sách sản phẩm được khuyến nghị
-recommended_items = [pred.iid for pred in top_k_predictions]
-st.write("Top", k, "sản phẩm được khuyến nghị:")
-st.write(data[data['itemID'].isin(recommended_items)][['itemID', 'itemName']])
+# # Hiển thị danh sách sản phẩm được khuyến nghị
+# recommended_items = [pred.iid for pred in top_k_predictions]
+# st.write("Top", k, "sản phẩm được khuyến nghị:")
+# st.write(data[data['itemID'].isin(recommended_items)][['itemID', 'itemName']])
