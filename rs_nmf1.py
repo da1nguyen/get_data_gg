b67b3393b09f532e2d3de00f629a1ff83a0fc0a6
Thon-Da Nguyen <67877012+da1nguyen@users.noreply.github.com>
2023-06-13 15:05:04 +0700

    Update rs_nmf1.py

--- rs_nmf1.py
+++ rs_nmf1.py
@@ -3,6 +3,7 @@ import streamlit as st
 import requests
 import io
 from surprise import Dataset, Reader, NMF
+from surprise.model_selection import cross_validate
 
 # Khai báo URL dữ liệu
 data_url = 'https://drive.google.com/uc?id=1IXbptj9A5VD-yHh8I_70SZcv2hi8NY2e'
@@ -16,26 +17,19 @@ assert response.status_code == 200, 'Could not download the data'
 # Đọc dữ liệu vào DataFrame
 data = pd.read_csv(io.StringIO(response.content.decode('utf-8')))
 
-# Chỉ lấy các cột reviewerID, asin, overall
-data = data[['reviewerID', 'asin', 'overall']]
-
 # Hiển thị DataFrame
-st.write(data)
+st.dataframe(data)
 
-# Lấy ID người dùng từ người dùng nhập vào
-user_id = st.text_input("Nhập ID người dùng:")
-
-# Nhập số lượng sản phẩm khuyến nghị
-k = int(st.text_input("Nhập số lượng sản phẩm khuyến nghị:"))
-
-# Tạo một đối tượng Reader để định dạng dữ liệu
+# Xây dựng mô hình khuyến nghị
 reader = Reader(rating_scale=(1, 5))
-
-# Tạo một đối tượng Dataset từ DataFrame
-dataset = Dataset.load_from_df(data, reader)
-
-# Xây dựng mô hình NMF với số lượng yếu tố latents = 10
+dataset = Dataset.load_from_df(data[['reviewerID', 'asin', 'overall']], reader)
 model = NMF(n_factors=10)
+cross_validate(model, dataset, measures=['RMSE', 'MAE'], cv=5, verbose=True)
+
+# Nhập thông tin từ người dùng
+user_id = st.text_input("Nhập ID người dùng:")
+k = st.text_input("Nhập số lượng sản phẩm khuyến nghị:")
+k = int(k) if k.isnumeric() else 0
 
 # Đào tạo mô hình trên toàn bộ dữ liệu
 trainset = dataset.build_full_trainset()
@@ -53,5 +47,8 @@ top_k_predictions = sorted(predictions, key=lambda x: x.est, reverse=True)[:k]
 # Hiển thị danh sách sản phẩm được khuyến nghị
 recommended_items = [pred.iid for pred in top_k_predictions]
 recommended_df = data[data['asin'].isin(recommended_items)]
-st.write("Top", k, "sản phẩm được khuyến nghị:")
-st.write(recommended_df[['asin', 'overall']])
+recommended_df = recommended_df[['asin', 'overall']]
+recommended_df = recommended_df.drop_duplicates()
+
+st.write(f"Top {k} sản phẩm được khuyến nghị:")
+st.dataframe(recommended_df)
