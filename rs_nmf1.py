2e4b958bff5350489e2bbd4f0841a655d3a8f78c
Thon-Da Nguyen <67877012+da1nguyen@users.noreply.github.com>
2023-06-13 14:29:20 +0700

    Update rs_nmf1.py

--- rs_nmf1.py
+++ rs_nmf1.py
@@ -1,8 +1,8 @@
+# Khai báo các thư viện
 import pandas as pd
 import streamlit as st
-import gdown
+import requests
 import io
-from surprise import Dataset, Reader, NMF
 
 # Chỉ ra dữ liệu (ở đây chứa một file csv)
 data_url = 'https://drive.google.com/uc?id=1MHLvwXQMgRKz9BMYqNE-NxPVUfoEmoYJ'
@@ -12,6 +12,10 @@ response = requests.get(data_url)
 
 # Kiểm tra xem link có thể nhận về trực tiếp hay không
 assert response.status_code == 200, 'Could not download the data'
+
+# Đọc dữ liệu vào DataFrame
+data = pd.read_csv(io.StringIO(response.content.decode('utf-8')))
+pd.set_option('display.max_colwidth', None)
 
 
 # Tạo một đối tượng Reader để định dạng dữ liệu
